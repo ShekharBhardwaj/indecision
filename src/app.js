868be2fd1963778219f5c17a7e2ac4aec086406c
shekharbhardwaj <shekhar.nik@gmail.com>
2018-08-27 01:23:29 -0400

    Adding babel-plugin-transform-class-properties

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import IndecisionApp from './components/IndecisionApp'
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+class Oldsyntax {
+    constructor () {
+        this.name = 'mike'
+    };
+
+    getGreeting () {
+        return `hi ${this.name}`
+    }
+}
+
+
+const oldsyntax = new Oldsyntax();
+console.log(oldsyntax.getGreeting());
+
+
+class NewSysntax {
+    name = 'Jen';
+    getGreeting = () => {
+        return `hi ${this.name}`
+    }
+}
+const newSysntax = new NewSysntax();
+console.log(newSysntax);
+
+
+
